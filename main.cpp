bc306c053e1e0b04d647bab7a3ad04833a2dec8c
Ashleigh Baumgardner <lash@boob.local>
2010-06-18 20:55:50 -0700

    Ugle hack to get the code running again. Everythin had been explictitly pasted or included in main.

--- main.cpp
+++ main.cpp
@@ -2,8 +2,60 @@
 #include <iomanip>
 #include <fstream>
 #include <locale>
-#include "header.hpp"
-#include "lists.cpp"
+#include <list>
+//#include "header.hpp"
+#include "peptide.cpp"
+#include "peptide.hpp"
+#include "protein.cpp"
+#include "protein.hpp"
+//#include "lists.cpp"
+using namespace std;
+
+list<Protein> proteinList;
+list<Protein>::iterator proteinIter;
+
+list<Peptide> peptideList;
+list<Peptide>::iterator peptideIter;
+
+void printProteins() {
+  int i = 0;
+  cout << endl;
+  for( proteinIter = proteinList.begin(); proteinIter != proteinList.end(); ++proteinIter) {
+      cout << i << "  " << (*proteinIter).name.substr(0, 7) << endl << (*proteinIter).sequence << endl << endl;
+      i++;
+  }
+}
+
+int badChar(char c) {
+    if( c == '*' )
+        return true;
+    else
+        return false;
+}
+
+void checkForSpecialChar(char c, int &numPTS, int &numM) {
+    if( (c == 'P') || (c == 'S') || (c == 'T')) 
+        numPTS++;
+    else if( (c == 'M') )
+        numM++;
+}
+
+int endPeptide( string &pepSeq) {
+    if( pepSeq[pepSeq.length()-1] == 'K' )
+        return true;
+    else
+        return false;
+}
+
+double massofPep( string seq ) {
+    double mass = 0;
+    for( int i = 0; i < seq.length(); i++) {
+        //mass += aminoAcidMass( seq[i] );
+        cout << "..." << endl;
+    }
+    return mass;
+}
+
 
 void getProteins(ifstream &inputStream) {
   string line;          // a string

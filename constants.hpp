6569f4b70662f5e8baa6b018f17eca3bc34d04aa
Ashleigh Baumgardner <lash@boob.local>
2010-06-19 15:55:43 -0700

    Added another test file

---
+++ constants.hpp
@@ -0,0 +1,9 @@
+#define MIN_PEPTIDE_MASS    500.0
+#define MAX_PEPTIDE_MASS    6000
+#define ALLOWED_MISSED_CLEAVAGES  2
+#define MAX_LEN_PEPTIDE     100
+#define MIN_LEN_PEPTIDE     6
+#define MAX_LEN_DEFINITION  50
+#define INITIAL_SEQ_LEN     1000
+#define MAX_LEN_PROTEIN     5000
+#define SEMI_TRYPTIC        false
